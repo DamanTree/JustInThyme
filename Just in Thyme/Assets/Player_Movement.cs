3a063800498490fb5b2bd415a5d2d0ced8c46c86
Daman Yang <19yangd@episcopalhighschool.org>
2019-04-20 19:54:17 -0400

    pls work

---
+++ Just in Thyme/Assets/Player_Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Translate(-1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.W)) 
+        {
+            transform.Translate(0, 1, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.D)) {
+            transform.Translate(1, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
+}
