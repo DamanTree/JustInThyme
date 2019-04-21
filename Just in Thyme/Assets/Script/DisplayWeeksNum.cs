0ee7b2425496ebb59c9a15143ca9e752b2d521d1
Lilian Chu <lilian.emchu@gmail.com>
2019-04-20 22:49:51 -0700

    Time Mechanic Progress
    -Added sliders and more info display to the UI
    -Still have to make the time mechanics change the environment

---
+++ Just in Thyme/Assets/Script/DisplayWeeksNum.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayWeeksNum : MonoBehaviour
+{
+    public Slider slider;
+
+    public Text display;
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.onValueChanged.AddListener(OnValueChange);
+    }
+
+    public void OnValueChange(float value)
+    {
+        display.text = "Weeks: " + value.ToString();
+    }
+}
