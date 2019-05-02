dd86c8780188422ad0cbf246a02a8410061c9219
kyluo <luokaiyuan@savcds.org>
2019-05-01 22:13:25 -0400

    final2

---
+++ Just in Thyme/Assets/Game Scene/script/intro.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class intro : MonoBehaviour
+{
+    public GameObject introduction;
+    // Start is called before the first frame update
+    public void clickintro()
+    {
+        introduction.SetActive(false);
+    }
+}
